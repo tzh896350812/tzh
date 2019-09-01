00ef668afca25728479989a4205dd5bf6742c8b8
th <896350812@qq.com>
2019-09-01 23:15:29 +0800

    初始化

---
+++ src/main/java/com/bw/bean/User.java
@@ -0,0 +1,22 @@
+package com.bw.bean;
+
+public class User {
+	
+	private int uid;
+	private String uname;
+	public int getUid() {
+		return uid;
+	}
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+	public String getUname() {
+		return uname;
+	}
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+	
+	
+
+}
---
+++ src/test/java/com/test/TestDemo.java
@@ -0,0 +1,11 @@
+package com.test;
+
+import org.junit.Test;
+
+public class TestDemo {
+
+	@Test
+	public void test1() {
+		System.out.println("sssss");
+	}
+}
