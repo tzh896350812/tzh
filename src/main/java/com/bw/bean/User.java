1d63c384022a683c43a533413c702a20bfd5a849
tzh <896350812@qq.com>
2019-09-03 00:11:52 +0800

    cccc

--- src/main/java/com/bw/bean/User.java
+++ src/main/java/com/bw/bean/User.java
@@ -16,11 +16,5 @@ public class User {
 	public void setUname(String uname) {
 		this.uname = uname;
 	}
-	@Override
-	public String toString() {
-		return "User [uid=" + uid + ", uname=" + uname + "]";
-	}
-
-	
 	
 }
